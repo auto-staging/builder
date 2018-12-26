67451b13f42dad557605bbea0894ed95f7212a43
Jan Ritter <jan-ritter@outlook.com>
2018-12-26 04:17:30 +0100

    docs: added go doc for types

--- types/typedef.go
+++ types/typedef.go
@@ -1,11 +1,13 @@
 package types
 
+// EnvironmentVariable is the implementation of the TowerAPI EnvironmentVariable schema
 type EnvironmentVariable struct {
 	Name  string `json:"name"`
 	Type  string `json:"type"`
 	Value string `json:"value"`
 }
 
+// Event contains the event body used in the invokation of the Lambda
 type Event struct {
 	Repository            string                `json:"repository"`
 	Branch                string                `json:"branch"`
@@ -18,27 +20,33 @@ type Event struct {
 	StartupSchedules      []TimeSchedule        `json:"startupSchedules"`
 }
 
+// TimeSchedule is the implementation of the TowerAPI TimeSchedule schema
 type TimeSchedule struct {
 	Cron string `json:"cron"`
 }
 
-type StatusUpdate struct {
-	Status string `json:":status"`
-}
-
+// Status struct contains the Environment status
 type Status struct {
 	Status string `json:"status"`
 }
 
+// StatusUpdate struct is used for DynamoDB updates, because the update command requires all json keys to start with ":"
+type StatusUpdate struct {
+	Status string `json:":status"`
+}
+
+// Build struct represents the build step of the CodeBuild buildspec
 type Build struct {
 	Commands []string
 	Finally  []string
 }
 
+// Phases struct represents the phases block of the CodeBuild buildspec
 type Phases struct {
 	Build Build
 }
 
+// Buildspec struct represents the general CodeBuild buildspec structure
 type Buildspec struct {
 	Version string
 	Phases  Phases
