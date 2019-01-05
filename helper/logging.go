073664656ded53a48a638db684d33a73b247a9ba
Jan Ritter <jan-ritter@outlook.com>
2019-01-05 04:40:45 +0100

    refactor: added missing error handling

--- helper/logging.go
+++ helper/logging.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"log"
 	"os"
 	"strconv"
 
@@ -12,6 +13,14 @@ var Logger *lightning.Lightning
 
 // Init is used to initalize Lightning Logger with the configured LogLevel.
 func Init() {
-	logLevel, _ := strconv.Atoi(os.Getenv("CONFIGURATION_LOG_LEVEL"))
-	Logger, _ = lightning.Init(logLevel)
+	logLevel, err := strconv.Atoi(os.Getenv("CONFIGURATION_LOG_LEVEL"))
+	if err != nil {
+		log.Println("Init() - Couldn't convert logLevel")
+		log.Println(err)
+	}
+	Logger, err = lightning.Init(logLevel)
+	if err != nil {
+		log.Println("Init() - Couldn't init Logger")
+		log.Println(err)
+	}
 }
--- model/update.go
+++ model/update.go
@@ -67,7 +67,10 @@ func AdaptCodeBildJobForUpdate(event types.Event) error {
 	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
 	if err != nil {
 		helper.Logger.Log(err, map[string]string{"module": "model/CreateCodeBuildJob", "operation": "regex/compile"}, 0)
-		setStatusForEnvironment(event, "updating failed")
+		errStatus := setStatusForEnvironment(event, "updating failed")
+		if errStatus != nil {
+			return errStatus
+		}
 		return err
 	}
 
