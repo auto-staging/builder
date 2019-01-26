9b73b90f43037fc778dce4c21298f3bbc526bafc
Jan Ritter <jan-ritter@outlook.com>
2019-01-26 13:10:40 +0100

    chore: added version information logging on startup

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ prepare:
 	dep ensure -v
 
 build: prepare
-	go build -o ./bin/builder -v
+	go build -o ./bin/auto-staging-builder -v -ldflags "-X github.com/auto-staging/builder/helper.commitHash=`git rev-parse HEAD` -X github.com/auto-staging/builder/config.buildTime=`date -u +"%Y-%m-%dT%H:%M:%SZ"` -X github.com/auto-staging/builder/config.branch=`git rev-parse --abbrev-ref HEAD` -X github.com/auto-staging/builder/config.version=`git describe --abbrev=0 --tags`"
 
 tests:
 	go test ./... -v
--- helper/logging.go
+++ helper/logging.go
@@ -11,16 +11,23 @@ import (
 // Logger contains the Lightning Logger instance configured by the Init function, it's used for logging by calling the Log function on it.
 var Logger *lightning.Lightning
 
+var version string
+var commitHash string
+var branch string
+var buildTime string
+
 // Init is used to initalize Lightning Logger with the configured LogLevel.
 func Init() {
+	log.Printf("version - %s | branch - %s | commit hash - %s | build time - %s \n", version, branch, commitHash, buildTime)
+
 	logLevel, err := strconv.Atoi(os.Getenv("CONFIGURATION_LOG_LEVEL"))
 	if err != nil {
-		log.Println("Init() - Couldn't convert logLevel")
+		log.Println("ERROR - Init() - Couldn't convert logLevel")
 		log.Println(err)
 	}
 	Logger, err = lightning.Init(logLevel)
 	if err != nil {
-		log.Println("Init() - Couldn't init Logger")
+		log.Println("ERROR - Init() - Couldn't init Logger")
 		log.Println(err)
 	}
 }
